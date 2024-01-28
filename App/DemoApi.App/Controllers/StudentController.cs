dfeddeda6d84bb9f0b418aa990561b61c75bcf1f
Rajdip27 <srajdip920@gmail.com>
2024-01-28 17:43:22 +0600

    Add Student Controller ANd List Show and Insert Complete

--- App/DemoApi.App/Controllers/StudentController.cs
+++ App/DemoApi.App/Controllers/StudentController.cs
@@ -1,5 +1,6 @@
 ﻿using DemoApi.App.Controllers.Base;
 using DemoApi.Application.Features.StudentOperation.Command;
+using DemoApi.Application.Features.StudentOperation.Query;
 using DemoApi.Application.ViewModel;
 using Microsoft.AspNetCore.Mvc;
 
@@ -9,4 +10,10 @@ public class StudentController : ApiControllerBase
 {
     [HttpPost]
     public async Task<ActionResult<StudentVm>> InsertAsync([FromBody] StudentVm model) => await HandelCommandAsync(new CreateStudent(model));
+
+    [HttpGet]
+    public async Task<ActionResult<StudentVm>> GetListAsync(int pageSize = 10, int pageIndex = 0, string searchText = null)
+    {
+        return await HandelQueryAsync(new GetAllStudentListAsync(pageSize, pageIndex, searchText));
+    }
 }
---
+++ DemoApi.Application/Features/StudentOperation/Query/GetAllStudentListAsync.cs
@@ -0,0 +1,39 @@
+﻿using AutoMapper;
+using DemoApi.Application.Repositories;
+using DemoApi.Application.ViewModel;
+using DemoApi.Domain.Extensions.Pagging;
+using DemoApi.Domain.Extensions.Results;
+using DemoApi.Domain.Model;
+using MediatR;
+
+namespace DemoApi.Application.Features.StudentOperation.Query;
+
+public record GetAllStudentListAsync(int PageSize, int PageIndex, string SearchText):IRequest<QueryResult<Paging<StudentVm>>>;
+
+public class GetAllStudentListAsyncHandler : IRequestHandler<GetAllStudentListAsync, QueryResult<Paging<StudentVm>>>
+{
+    private readonly IStudentRepository _studentRepository;
+    private readonly IMapper _mapper;
+
+    public GetAllStudentListAsyncHandler(IStudentRepository studentRepository, IMapper mapper)
+    {
+        _studentRepository = studentRepository;
+        _mapper = mapper;
+    }
+
+    public async Task<QueryResult<Paging<StudentVm>>> Handle(GetAllStudentListAsync request, CancellationToken cancellationToken)
+    {
+      var result = await _studentRepository.GetPageAsync(request.PageIndex, request.PageSize,
+      p => (string.IsNullOrEmpty(request.SearchText) | p.Name.Contains(request.SearchText)),
+      o => o.OrderBy(o => o.Id),
+      se => se);
+
+        var data = result.ToPagingModel<Student, StudentVm>(_mapper);
+
+        return data switch
+        {
+            not null => new QueryResult<Paging<StudentVm>>(data, QueryResultTypeEnum.Success),
+            _ => new QueryResult<Paging<StudentVm>>(null, QueryResultTypeEnum.NotFound)
+        };
+    }
+}
