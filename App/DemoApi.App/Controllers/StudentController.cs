ddd5b6df380063033b822dc94c71cc33dd2d9f0b
Rajdip27 <srajdip920@gmail.com>
2024-01-28 17:43:19 +0600

    Add Student Controller ANd List Show and Insert Complete

---
+++ App/DemoApi.App/Controllers/StudentController.cs
@@ -0,0 +1,12 @@
+﻿using DemoApi.App.Controllers.Base;
+using DemoApi.Application.Features.StudentOperation.Command;
+using DemoApi.Application.ViewModel;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DemoApi.App.Controllers;
+
+public class StudentController : ApiControllerBase
+{
+    [HttpPost]
+    public async Task<ActionResult<StudentVm>> InsertAsync([FromBody] StudentVm model) => await HandelCommandAsync(new CreateStudent(model));
+}
