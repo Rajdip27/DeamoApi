894671919aa023ade2c48351b540b92baf7fca65
Rajdip27 <srajdip920@gmail.com>
2024-01-29 17:42:58 +0600

    Add Country Controller

---
+++ App/DemoApi.App/Controllers/CountryController.cs
@@ -0,0 +1,19 @@
+﻿using DemoApi.App.Controllers.Base;
+using DemoApi.Application.Features.CountryOperation.Command;
+using DemoApi.Application.ViewModel;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DemoApi.App.Controllers;
+
+
+public class CountryController : ApiControllerBase 
+{
+    [HttpPost]
+    public async Task<ActionResult<CountryVm>> CreateCountry([FromBody] CountryVm countryVm) =>
+        await  HandelCommandAsync(new CreateCountry(countryVm));
+
+
+
+}
+
+
