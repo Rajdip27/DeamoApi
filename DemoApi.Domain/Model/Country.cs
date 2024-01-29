083398b200a4f1087b265de6528ee1394f62d86b
Rajdip27 <srajdip920@gmail.com>
2024-01-29 16:12:02 +0600

    Create country Table

---
+++ DemoApi.Domain/Model/Country.cs
@@ -0,0 +1,8 @@
+﻿using DemoApi.Domain.Model.BaseEntities;
+
+namespace DemoApi.Domain.Model;
+
+public class Country: AuditableEntity
+{
+    public string Name { get; set; }
+}
---
+++ DemoApi.Infrastructure/EntityConfiguration/CountryConfiguration.cs
@@ -0,0 +1,15 @@
+﻿using DemoApi.Domain.Model;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace DemoApi.Infrastructure.EntityConfiguration;
+
+public class CountryConfiguration : IEntityTypeConfiguration<Country>
+{
+    public void Configure(EntityTypeBuilder<Country> builder)
+    {
+        builder.ToTable(name: nameof(Country));
+        builder.HasKey(x => x.Id);
+        builder.Property(x=>x.Name).HasMaxLength(50).IsRequired(true);
+    }
+}
