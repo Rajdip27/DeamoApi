606a6c2674a916fabd58dcfd3744c1e554405bc7
Rajdip27 <srajdip920@gmail.com>
2024-01-31 10:58:09 +0600

    GetCountryDropdownAsync done

--- DemoApi.Application/ViewModel/CountryVm.cs
+++ DemoApi.Application/ViewModel/CountryVm.cs
@@ -1,10 +1,11 @@
 ﻿using AutoMapper;
 using DemoApi.Domain.Model;
+using DemoApi.Domain.Model.BaseEntities;
 
 namespace DemoApi.Application.ViewModel;
-[AutoMap(typeof(Country),ReverseMap =true)]
-public record CountryVm(
-    long Id,
-    string Name
-);
+[AutoMap(typeof(Country), ReverseMap = true)]
+public class CountryVm : BaseEntity
+{
+    public string Name { get; set; }
+}
 
