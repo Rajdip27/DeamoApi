d926c75cc2d57e5d31bc4961630fc391f7a56e80
Rajdip27 <srajdip920@gmail.com>
2024-02-01 16:57:09 +0600

    Create State Controller Create

---
+++ DemoApi.Application/Repositories/IStateRepository.cs
@@ -0,0 +1,9 @@
+﻿using DemoApi.Application.Repositories.Base;
+using DemoApi.Application.ViewModel;
+using DemoApi.Domain.Model;
+
+namespace DemoApi.Application.Repositories;
+
+public interface IStateRepository:IBaseRepository<State,StateVm,long>
+{
+}
