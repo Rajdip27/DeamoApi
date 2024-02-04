cac7c29bb2bf7aa5d216c744bb03573757bee8e6
Rajdip27 <srajdip920@gmail.com>
2024-02-04 10:51:24 +0600

    Create State Done

--- App/DemoApi.App/Controllers/StateController.cs
+++ App/DemoApi.App/Controllers/StateController.cs
@@ -1,5 +1,6 @@
 ﻿using DemoApi.App.Controllers.Base;
-using Microsoft.AspNetCore.Http;
+using DemoApi.Application.Features.StateOperation.Command;
+using DemoApi.Application.ViewModel;
 using Microsoft.AspNetCore.Mvc;
 
 namespace DemoApi.App.Controllers;
@@ -7,4 +8,7 @@ namespace DemoApi.App.Controllers;
 
 public class StateController : ApiControllerBase
 {
+    public async Task<ActionResult<StateVm>> CreatState(StateVm model)=>
+        await HandelCommandAsync(new CreateState(model));
+   
 }
