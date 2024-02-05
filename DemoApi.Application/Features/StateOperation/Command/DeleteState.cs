7cd374a3d9274a1fc6968ce998e299500e135615
Rajdip27 <srajdip920@gmail.com>
2024-02-05 12:10:49 +0600

    Delete Done

---
+++ DemoApi.Application/Features/StateOperation/Command/DeleteState.cs
@@ -0,0 +1,44 @@
+﻿using DemoApi.Application.Repositories;
+using DemoApi.Application.ViewModel;
+using DemoApi.Domain.Extensions.Results;
+using FluentValidation;
+using MediatR;
+
+namespace DemoApi.Application.Features.StateOperation.Command;
+
+public record DeleteState(long id):IRequest<CommandResult<StateVm>>;
+public class DeleteStateHandler : IRequestHandler<DeleteState, CommandResult<StateVm>>
+{
+    private readonly IStateRepository stateRepository;
+    private readonly IValidator<DeleteState> validator;
+
+    public DeleteStateHandler(IStateRepository stateRepository, IValidator<DeleteState> validator)
+    {
+        this.stateRepository = stateRepository;
+        this.validator = validator;
+    }
+
+    public async Task<CommandResult<StateVm>> Handle(DeleteState request, CancellationToken cancellationToken)
+    {
+        var validationResult= await validator.ValidateAsync(request, cancellationToken);
+        if(validationResult is not null)
+        {
+            var data = await stateRepository.DeleteAsync(request.id);
+            return data switch
+            {
+                null => new CommandResult<StateVm>(null,CommandResultTypeEnum.NotFound),
+                _=> new CommandResult<StateVm>(data,CommandResultTypeEnum.Success)
+
+            };
+        }
+        throw new ValidationException(validationResult.Errors);
+    }
+}
+public class DeleteStateValidation : AbstractValidator<DeleteState>
+{
+    public DeleteStateValidation()
+    {
+        RuleFor(x=>x.id).NotNull().WithMessage("'{PropertyName}' information is required.");
+    }
+}
+
